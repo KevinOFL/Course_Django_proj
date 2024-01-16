f0d5bc6a25446a4a9294a0ef3b23b42ed0d71c56
Kevin Silva <kevinsilvaofl@gmail.com>
2024-01-16 10:37:25 -0300

    Update and share the html code, add a page for a single recipe and configure the urls.

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', status=200, context={
-        'name': 'Jamal',
-    })
+    return render(request, 'recipes/pages/home.html', status=200)
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', status=200)
 
