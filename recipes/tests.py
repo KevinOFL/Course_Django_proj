b80579f8b62643cc2ef62ad50714a9562b912286
Kevin Silva <kevinsilvaofl@gmail.com>
2024-01-24 17:24:00 -0300

    File created for pytest and its configurations

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+class RecipeURLsTest(TestCase):
+    def test_the_pytest_is_ok(self):
+        num = 0
+        assert 1 == 1
+        num += 1 == 2
