d2b9a67e13971fd47f4e11088d5db61204ddc5aa
Kevin Silva <kevinsilvaofl@gmail.com>
2024-02-06 16:29:43 -0300

    Tests for model Category

--- recipes/tests/test_recipe_models_recipe.py
+++ recipes/tests/test_recipe_models_recipe.py
@@ -54,3 +54,14 @@ class RecipeModelsTest(RecipeTestBase):
             recipe.is_published,
             msg='Recipe is_published is not False'
         )
+
+    def test_recipe_string_representation(self):
+        needed = 'Testing representation'
+        self.recipe.title = 'Testing representation'
+        self.recipe.full_clean()
+        self.recipe.save()
+        self.assertEqual(
+            str(self.recipe),
+            needed,
+            msg=f'Recipe string representation must be '
+                f'"{needed}" but "{str(self.recipe)}" was received.')
