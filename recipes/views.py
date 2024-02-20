f8b59fde6dc4b9f810f485c0a6714dba2d43dce8
Kevin Silva <kevinsilvaofl@gmail.com>
2024-02-20 16:28:26 -0300

    Configrated ambient variable and new test for ambiente variable

--- recipes/views.py
+++ recipes/views.py
@@ -2,15 +2,21 @@ from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.http import Http404
 from django.db.models import Q
 from django.core.paginator import Paginator
-from recipes.models import Recipe
 from utils.pagination import make_pagination
+import os
+
+from recipes.models import Recipe
+
+
+PER_PAGE = int(os.environ.get('PER_PAGE', 9))
+
 
 def home(request):
     recipes = Recipe.objects.filter(
         is_published=True
     ).order_by('-id')
     
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/home.html', context={
         'recipes': page_obj,
@@ -25,7 +31,7 @@ def category(request, category_id):
        ).order_by('-id')
     )
 
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     
     return render(request, 'recipes/pages/category.html', context={
@@ -56,7 +62,7 @@ def search(request):
         is_published = True
     ).order_by('-id')
 
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     
     return render(request, 'recipes/pages/search.html', {
