72bceff03649baeabcc35a3e0c931715e79857b3
Kevin Silva <kevinsilvaofl@gmail.com>
2024-02-20 12:14:06 -0300

    Configuring view urls with pagination

--- recipes/views.py
+++ recipes/views.py
@@ -3,27 +3,15 @@ from django.http import Http404
 from django.db.models import Q
 from django.core.paginator import Paginator
 from recipes.models import Recipe
-from utils.pagination import make_pagination_range
+from utils.pagination import make_pagination
 
 def home(request):
     recipes = Recipe.objects.filter(
         is_published=True
     ).order_by('-id')
     
-    try: 
-        current_page = int(request.GET.get('page', 1))
-    except ValueError:    
-        current_page = 1
+    page_obj, pagination_range = make_pagination(request, recipes, 9)
 
-    paginator = Paginator(recipes, 9)
-    page_obj = paginator.get_page(current_page)
-
-
-    pagination_range = make_pagination_range(
-        paginator.page_range,
-        4,
-        current_page
-    )
     return render(request, 'recipes/pages/home.html', context={
         'recipes': page_obj,
         'pagination_range': pagination_range
@@ -36,9 +24,13 @@ def category(request, category_id):
         is_published=True
        ).order_by('-id')
     )
+
+    page_obj, pagination_range = make_pagination(request, recipes, 9)
+
     
     return render(request, 'recipes/pages/category.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
         'title': f'{recipes[0].category.name} - Category |'
     })
 
@@ -56,18 +48,21 @@ def search(request):
     if not search_term:
         raise Http404()
     
-    recipe = Recipe.objects.filter(
+    recipes = Recipe.objects.filter(
         Q(
             Q(title__icontains=search_term) |
             Q(description__icontains=search_term),
         ),
         is_published = True
-    )
-    recipe = recipe.filter(is_published=True)
-    recipe = recipe.order_by('-id')
+    ).order_by('-id')
+
+    page_obj, pagination_range = make_pagination(request, recipes, 9)
+
     
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" |',
         'search_term': search_term,
-        'recipes': recipe,
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
+        'additional_url_query': f'&q={search_term}',
     })
