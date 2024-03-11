eb20411f98265518f20bc7429e14211ebe12f67e
Kevin Silva <kevinsilvaofl@gmail.com>
2024-03-11 14:44:08 -0300

    Finalizing registration testing and form validation

--- authors/tests/test_author_register_form.py
+++ authors/tests/test_author_register_form.py
@@ -135,3 +135,19 @@ class AuthorRegisterFormIntegrationTest(DjangoTestCase):
         self.assertEqual(response.status_code, 404)
         self.assertNotEqual(response.status_code, 200)
         
+    def test_email_field_must_be_unique(self):
+        url = reverse('authors:create')
+        
+        self.client.post(url, data=self.form_data, follow=True)
+        response = self.client.post(url, data=self.form_data, follow=True)
+        
+        msg = 'User e-mail is already in use'
+        
+        self.assertIn(
+            msg,
+            response.context['form'].errors.get('email')
+        )
+        self.assertIn(msg, response.content.decode('utf-8'))
+
+        
+        
