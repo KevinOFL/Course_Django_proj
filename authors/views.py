2ea306ecd50a36d39ddc8910fab15032387bdab0
Kevin Silva <kevinsilvaofl@gmail.com>
2024-03-12 15:13:42 -0300

    Desenvolved function at the logout user

--- authors/views.py
+++ authors/views.py
@@ -3,7 +3,8 @@ from .forms import RegisterForm, LoginForm
 from django.http import Http404
 from django.contrib import messages
 from django.urls import reverse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 def register_view(request):
     register_form_data = request.session.get('register_form_data', None)
@@ -48,7 +49,7 @@ def login_create(request):
     if form.is_valid():
         authenticated_user = authenticate(
             username= form.cleaned_data.get('username', ''),
-            pasword= form.cleaned_data.get('password', '')
+            password= form.cleaned_data.get('password', ''),
         )
         
         if authenticated_user is not None:
@@ -58,4 +59,17 @@ def login_create(request):
             messages.error(request, 'Invalid credentials')
     else:    
         messages.error(request, 'Invalid username or password')
-        return redirect(login_url)
+    
+    return redirect(login_url)
+    
+@login_required(login_url='authors:login', redirect_field_name='next')    
+def logout_view(request):
+    if not request.POST:
+        return redirect(reverse('authors:login'))
+    
+    if request.POST.get('username') != request.user.username:
+        return redirect(reverse('authors:login'))
+    
+    logout(request)
+    return redirect(reverse('authors:login'))
+    
